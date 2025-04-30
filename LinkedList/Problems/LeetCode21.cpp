c347c81ff327ee206d2aed40b2195035d2950a47
ankit-prabhavak <ankitabcd1718@gmail.com>
2025-04-30 23:19:57 +0530

    added two leetcode linked list problems

---
+++ LinkedList/Problems/LeetCode21.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (list1 == NULL || list2 == NULL) {
+            return list1 == NULL ? list2 : list1;
+        }
+
+        if (list1->val <= list2->val) {
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        } else {
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
+        }
+    }
+};
+
+// Helper function to print the linked list
+void printList(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    // Create first sorted linked list: 1 -> 2 -> 4
+    ListNode* list1 = new ListNode(1);
+    list1->next = new ListNode(2);
+    list1->next->next = new ListNode(4);
+
+    // Create second sorted linked list: 1 -> 3 -> 4
+    ListNode* list2 = new ListNode(1);
+    list2->next = new ListNode(3);
+    list2->next->next = new ListNode(4);
+
+    Solution s;
+    ListNode* mergedList = s.mergeTwoLists(list1, list2);
+
+    // Print the merged sorted linked list
+    cout << "Merged Sorted List: ";
+    printList(mergedList);
+
+    return 0;
+}
