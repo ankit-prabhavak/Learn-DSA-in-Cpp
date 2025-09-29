2a746a2946a6e40a3d1ed2de2ba98444958282d9
ankit-prabhavak <ankitabcd1718@gmail.com>
2025-09-29 12:20:40 +0530

    Solved ZigZag level order traversal

--- Trees/BST/Problems/LeetCode897.cpp
+++ Trees/BST/Problems/LeetCode897.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 
-
 using namespace std;
-
 
 /*
   Definition for a binary tree node.
@@ -15,7 +13,8 @@ struct TreeNode {
      TreeNode() : val(0), left(nullptr), right(nullptr) {}
      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- };
+};
+
 
 class Solution {
 public:
@@ -33,6 +32,7 @@ public:
     }
     
     TreeNode* insertNode(TreeNode* root, int val) {
+        
         if(root == NULL) return new TreeNode(val);
 
         if(val < root->val) {
