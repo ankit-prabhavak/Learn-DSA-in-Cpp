291beed4ac2b492d867159c463fc0c8737caeafc
ankit-prabhavak <ankitabcd1718@gmail.com>
2026-01-28 16:34:44 +0530

    Started Quick Recap

--- Array/Problems/MajorityElement.cpp
+++ Array/Problems/MajorityElement.cpp
@@ -43,3 +43,23 @@ int main(){
 
     return 0;
 }
+
+/*
+Dry Run 
+-------
+Input: [1 2 3 2 2 4]
+
+Iteration 1: i = 0, freq = 0 -> candidate = 1, freq = 1
+
+Iteration 2: i = 1, arr[i] = 2 != candidate -> freq = 0
+
+Iteration 3: i = 2, freq = 0 -> candidate = 3, freq = 1
+
+Iteration 4: i = 3, arr[i] = 2 != candidate -> freq = 0
+
+Iteration 5: i = 4, freq = 0 -> candidate = 2, freq = 1
+
+Iteration 6: i = 5, arr[i] = 4 != candidate -> freq = 0
+
+Output: 2 (majority element which is our last candidate)
+*/
