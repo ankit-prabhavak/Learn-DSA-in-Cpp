91cd4d004305727908fddd7f91bbdf677d46e0d8
ankit-prabhavak <ankitabcd1718@gmail.com>
2026-02-21 21:36:20 +0530

    Solved Leetcode 762

---
+++ BasicMaths/Problems/LeetCode762.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+
+class Solution {
+public:
+    int countPrimeSetBits(int left, int right) {
+        // Precomputed primes up to 20 (max bits for 10^6 is 19)
+        // Using a simple boolean array for O(1) lookup
+        bool primes[21] = {false};
+        for (int p : {2, 3, 5, 7, 11, 13, 17, 19}) {
+            primes[p] = true;
+        }
+
+        int answer = 0;
+        for (int i = left; i <= right; i++) {
+            int count = __builtin_popcount(i);
+            if (primes[count]) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+};
