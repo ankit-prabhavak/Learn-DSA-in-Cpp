9a780f047d2ad5b9e8cdfec08637410c4b73e017
ankit-prabhavak <ankitabcd1718@gmail.com>
2026-03-16 21:50:06 +0530

    Solved LeetCode 49 and updated DSA Sheet

---
+++ Strings/Problems/LeetCode49.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+// LeetCode 49: Group Anagrams
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        unordered_map<string, vector<string>> mp;
+
+        for(string s : strs) {
+            string key = s;
+            sort(key.begin(), key.end());
+            mp[key].push_back(s);
+        }
+
+        vector<vector<string>> result;
+
+        for(auto &p : mp) {
+            result.push_back(p.second);
+        }
+
+        return result;
+    }
+};
