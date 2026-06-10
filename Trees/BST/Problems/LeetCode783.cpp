4d820ef99173f36e55493ce0f241082b6c3f84f2
ankit-prabhavak <ankitabcd1718@gmail.com>
2026-06-10 14:14:31 +0530

    Solved Leetcode 783

--- Trees/BST/Problems/LeetCode783.cpp
+++ Trees/BST/Problems/LeetCode783.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <climits>
+#include <vector>
 using namespace std;
 
 /*
@@ -39,5 +40,52 @@ public:
            int rightMin = minDiffInBST(root->right);
            ans = min(ans, rightMin);
         }
+    }
+};
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* root, vector<int> &nums) {
+        if(!root) return;
+
+        if(root->left) {
+            dfs(root->left, nums);
+        }
+
+        if(root) {
+            nums.push_back(root->val);
+        }
+
+        if(root->right) {
+            dfs(root->right, nums);
+        }
+    }
+    int minDiffInBST(TreeNode* root) {
+
+        vector<int> nums;
+        dfs(root, nums);
+
+        int ans = INT_MAX;
+
+        for(int i = 0; i < nums.size()-1; i++) {
+
+            ans = min(ans, nums[i+1]-nums[i]);
+
+        }
+
+        return ans;
+        
     }
 };
